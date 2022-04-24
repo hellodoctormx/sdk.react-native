9fb31659fb9f8ae3dd0538140929b7863f2460e5
Daniel Sarfati <daniel@hellodoctor.com.mx>
2022-04-24 15:57:53 -0500

    fixup! checking in for 0.1.3

--- index.ts
+++ index.ts
@@ -1,236 +1,161 @@
-import uuid from "react-native-uuid-generator";
-import _ from "lodash";
-import {AppState, Platform, Vibration} from "react-native";
-import notifee from "@notifee/react-native";
-import RNCallKeep from "../callkeep";
-import VideoService from "../api/video";
-import * as activeCallManager from "./activeCallManager";
-import * as connectionService from "./connectionService";
-import {getIncomingCallNotification, getOngoingCallNotification} from "./notifications";
-import {navigateOnEndCall, tryNavigateOnIncomingCall} from "./eventHandlers";
+import * as auth from "./js/users/auth";
+import {getCurrentUser} from "./js/users/auth";
+import * as activeCallManager from "./js/telecom/activeCallManager";
+import * as connectionManager from "./js/telecom/connectionManager";
+import * as connectionService from "./js/telecom/connectionService";
+import * as eventHandlers from "./js/telecom/eventHandlers";
+import * as notifications from "./js/telecom/notifications";
+import HDCallKeep from "./js/callkeep";
+import HDVideoCall from "./js/components/HDVideoCall"
+import HDVideoCallView from "./js/components/HDVideoCallView"
+import HDVideoPermissionsConfiguration from "./js/components/HDVideoPermissionsConfiguration";
+import HDIncomingVideoCallView from "./js/components/HDIncomingVideoCallView"
+import PreviewLocalVideoView from "./js/components/PreviewLocalVideoView";
+import usersServiceApi from "./js/api/users";
+import videoServiceApi from "./js/api/video";
+import withVideoCallPermissions from "./js/components/withVideoCallPermissions";
+import Http from "./js/api/http";
 
-const calls = [];
-const callListeners = [];
+class HDUsers {
+    static createUser(accountPayload) {
+        return usersServiceApi.createThirdPartyUserAccount(accountPayload)
+    }
 
-const getNewCallUUID = async () => {
-    const callUUID = await uuid.getRandomUUID();
-    // have to toLowerCase because CallKeep internally lower cases that shit for whatever reason
-    return callUUID.toLowerCase();
-}
+    static deleteUser(userID) {
+        return usersServiceApi.deleteThirdPartyUserAccount(userID)
+    }
 
-export function getVideoCallStatus(videoRoomSID) {
-    const call = calls.find(c => c.videoRoomSID === videoRoomSID);
+    static updateMessagingToken(token) {
+        const currentUser = getCurrentUser()
 
-    return call?.status;
-}
+        if (currentUser === null) {
+            console.warn("[HDUsers:updateMessagingToken] can't update: no current user");
+            return;
+        }
 
-export function getCallByUUID(callUUID) {
-    return calls.find(c => c.uuid === callUUID);
-}
-
-export function getActiveCall() {
-    return _.find(calls, c => c.status === "in-progress");
-}
-
-export function getIncomingCall() {
-    return _.find(calls, c => c.status === "incoming") || getActiveCall();
-}
-
-export function registerCallStatusListener(videoRoomSID, listener) {
-    callListeners.push({videoRoomSID, listener});
-
-    const call = calls.find(c => c.videoRoomSID === videoRoomSID);
-
-    if (call) {
-        listener(call.status);
+        return usersServiceApi.updateThirdPartyUserMessagingToken(currentUser.uid, currentUser.deviceID, token)
     }
 }
 
-function notifyCallStatusListeners(videoRoomSID, status) {
-    callListeners.filter(l => l.videoRoomSID === videoRoomSID).forEach(l => l.listener(status));
+class HDConsultations {
+    static getConsultations(thirdPartyUserID, limit) {
+        // TODO probably want to use hd user ID, but things are tangled up with the third-party ID at the moment
+
+        // const currentUser = getCurrentUser()
+        //
+        // if (currentUser === null) {
+        //     console.warn("[HDConsultations:getConsultations] can't get consultations: no current user");
+        //     return;
+        // }
+
+        return usersServiceApi.getThirdPartyUserConsultations(thirdPartyUserID, limit);
+    }
 }
 
-export async function registerIncomingVideoCall(uuid, videoRoomSID, consultationID, caller) {
-    const call = {
-        uuid: uuid || await getNewCallUUID(),
-        videoRoomSID,
-        consultationID,
-        caller,
-        status: "incoming",
-        isCallMuted: false,
-        isCallHeld: false
-    };
-
-    calls.push(call);
-
-    notifyCallStatusListeners(videoRoomSID, call.status);
-
-    return call;
-}
-
-export async function notifyIncomingCall(incomingCall) {
-    // FIXME only skip iOS if PushKit not setup. So figure out how to determine that
-    if (Platform.OS === "ios") {
-        // PushKit calls RNCallKeep natively in iOS to display incoming calls
-        return;
+class HDVideoCalls {
+    static handleIncomingVideoCallNotification(videoCallPayload) {
+        return eventHandlers.handleIncomingVideoCall(videoCallPayload)
     }
 
-    console.info("[notifyIncomingCall]", incomingCall);
+    static handleIncomingVideoCallNotificationRejected() {
+        console.info("[HDVideoCalls:handleIncomingVideoCallNotificationRejected]");
 
-    const {videoRoomSID, consultationID, caller} = incomingCall;
-
-    if (Platform.OS === "android") {
-        console.info(`[handleIncomingVideoCall:notification] displaying incoming call notification ${videoRoomSID}:${incomingCall.uuid} | appState: ${AppState.currentState}`);
-
-        tryNavigateOnIncomingCall(consultationID, videoRoomSID);
-
-        await activeCallManager.startNotificationAlerts();
-
-        const incomingCallNotification = await getIncomingCallNotification(consultationID, videoRoomSID);
-
-        incomingCallNotificationIDs[videoRoomSID] = await notifee
-            .displayNotification(incomingCallNotification)
-            .catch(error => console.warn(`error displaying incoming call notification`, error));
-
-        console.info(`[notifyIncomingCall] displayed notification ${incomingCallNotificationIDs[videoRoomSID]}`);
-    } else {
-        console.info(`[notifyIncomingCall:CallKeep] displaying incoming call ${videoRoomSID}:${incomingCall.uuid} | appState: ${AppState.currentState}`);
-
-        RNCallKeep.displayIncomingCall(incomingCall.uuid, "HelloDoctor", caller.displayName || "HelloDoctor", "generic", true);
-
-    }
-
-    console.info("[notifyIncomingCall:DONE]");
-}
-
-export function handleIncomingVideoCallStarted(videoRoomSID) {
-    console.info(`[handleIncomingVideoCallStarted] videoRoomSID: ${videoRoomSID}`, videoRoomSID);
-
-    const call = calls.find(c => c.videoRoomSID === videoRoomSID);
-
-    if (!call) {
-        console.warn(`[handleIncomingVideoCallStarted] cannot start ${videoRoomSID}: no call found`);
-        return;
-    }
-
-    call.status = "in-progress";
-
-    tryCancelVideoCallNotification(videoRoomSID);
-}
-
-export function handleIncomingVideoCallAnswered(videoRoomSID) {
-    console.info(`[handleIncomingVideoCallAnswered] videoRoomSID: ${videoRoomSID}`, videoRoomSID);
-
-    // FIXME I don't think this try/catch is the best
-    try {
         activeCallManager.stopNotificationAlerts();
 
-        const call = calls.find(c => c.videoRoomSID === videoRoomSID);
+        const incomingCall = connectionManager.getIncomingCall();
 
-        call.status = "in-progress";
+        if (!incomingCall) {
+            console.info("[HDVideoCalls:handleIncomingVideoCallNotificationRejected] cannot reject incoming call: none found");
+            return;
+        }
 
-        tryCancelVideoCallNotification(videoRoomSID);
-    } catch(error) {
-        console.warn("WTF", error)
+        connectionManager.rejectVideoCall(incomingCall.videoRoomSID).catch(console.warn);
+    }
+
+    static handleIncomingVideoCallEndedRemotely(videoCallPayload) {
+        return eventHandlers.handleIncomingVideoCallEndedRemotely(videoCallPayload);
+    }
+
+    static startVideoCall(videoRoomSID) {
+        connectionManager.handleIncomingVideoCallStarted(videoRoomSID);
+
+        const listener = status => {
+            switch (status) {
+                case "completed":
+                case "rejected":
+                    this.endVideoCall(videoRoomSID).catch(console.warn);
+                    break;
+            }
+        }
+
+        connectionManager.registerCallStatusListener(videoRoomSID, listener);
+    }
+
+    static endVideoCall(videoRoomSID) {
+        return connectionManager.endVideoCall(videoRoomSID)
+    }
+
+    static getVideoCallAccessToken(videoRoomSID) {
+        return videoServiceApi
+            .requestVideoCallAccess(videoRoomSID)
+            .then(response => response.accessToken);
     }
 }
 
-const incomingCallNotificationIDs = {};
+export default class RNHelloDoctor {
+    static appName: string = null
+    static consultations: HDConsultations = HDConsultations
+    static users: HDUsers = HDUsers
+    static videos: HDVideoCalls = HDVideoCalls
 
-export function tryCancelVideoCallNotification(videoRoomSID) {
-    const incomingCallNotificationID = incomingCallNotificationIDs[videoRoomSID];
+    static async configure(appName, apiKey, config?: RNHelloDoctorConfig) {
+        RNHelloDoctor.appName = appName;
 
-    if (incomingCallNotificationID) {
-        notifee.cancelNotification(incomingCallNotificationID).catch(console.warn);
+        Http.API_KEY = apiKey;
+
+        if (config !== undefined && config.user) {
+            await auth.signIn(config.user.uid, config.user.deviceID, config.user.jwt, config.user.serverToken)
+        }
+
+        if (config !== undefined && config.video) {
+            connectionService.bootstrap(config.video).catch(error => console.warn("[RNHelloDoctor:configure]", {error}));
+        }
+    }
+
+    static teardown() {
+        connectionService.teardown();
     }
 }
 
-export async function endVideoCall(videoRoomSID) {
-    console.info(`[endConsultationVideoCall:${videoRoomSID}:START]`);
-
-    const call = _.find(calls, c => c.videoRoomSID === videoRoomSID);
-
-    const isCallKeepConfigured = await connectionService.checkIsCallKeepConfigured();
-
-    if (!call) {
-        console.warn(`no call found for room ${videoRoomSID}`);
-        return;
-    } else if (isCallKeepConfigured) {
-        RNCallKeep.endCall(call.uuid);
-    }
-
-    if (call.status === "completed") {
-        // no need to re-complete brah
-        console.info(`[endConsultationVideoCall:${videoRoomSID}]: call has already been completed`);
-        return;
-    }
-
-    call.status = "completed";
-
-    notifyCallStatusListeners(videoRoomSID, call.status);
-
-    await VideoService.endVideoCall(videoRoomSID).catch(error => console.warn(`error ending video call ${videoRoomSID}`, error));
-
-    navigateOnEndCall(call.consultationID, videoRoomSID);
-
-    console.info(`[endConsultationVideoCall:${videoRoomSID}:DONE]`);
+export interface RNHelloDoctorConfig {
+    user?: HDUserConfig,
+    video: HDVideoCallsConfig
 }
 
-export async function rejectVideoCall(videoRoomSID) {
-    const call = _.find(calls, c => c.videoRoomSID === videoRoomSID);
-
-    if (!call) {
-        console.warn(`no call found for room ${videoRoomSID}`);
-        return;
-    }
-
-    call.status = "completed";
-
-    notifyCallStatusListeners(videoRoomSID, call.status);
-
-    activeCallManager.stopNotificationAlerts();
-
-    await VideoService.rejectVideoCall(videoRoomSID);
-
-    RNCallKeep.rejectCall(call.uuid);
+export interface HDUser {
+    uid: string,
+    jwt?: string,
+    deviceID?: string
 }
 
-const incomingPushKitCall = {
-    uuid: null
-
+interface HDUserConfig extends HDUser {
+    serverToken: string
 }
 
-const unregisteredAnswerableCall = {
-    uuid: null
+interface HDVideoCallsConfig {
+    onAnswerCall: Function,
+    onEndCall: Function,
+    onIncomingCall?: Function
 }
 
-export function registerAnswerablePushKitCallUUID(pushKitCallUUID) {
-    unregisteredAnswerableCall.uuid = pushKitCallUUID;
-}
-
-export async function registerPushKitCall(notification) {
-    console.info("[connectionManager.registerPushKitCall]", {notification, unregisteredAnswerableCall});
-    const {uuid, videoRoomSID, consultationID, callerDisplayName, callerPhoneNumber, callerEmail} = notification;
-
-    incomingPushKitCall.uuid = uuid;
-
-    const existingCall = calls.find(c => c.videoRoomSID === videoRoomSID);
-
-    if (existingCall) {
-        console.info("already have call existing incoming call, skipping", existingCall);
-        return;
-    }
-
-    const caller = {
-        displayName: callerDisplayName,
-        phoneNumber: callerPhoneNumber,
-        email: callerEmail
-    }
-
-    await registerIncomingVideoCall(uuid, videoRoomSID, consultationID, caller);
-
-    // FIXME
-    // if (uuid === unregisteredAnswerableCall.uuid) {
-    //     navigateOnAnswerCall(consultationID, videoRoomSID);
-    // }
-}
+export {
+    connectionManager,
+    notifications,
+    HDCallKeep,
+    HDIncomingVideoCallView,
+    HDVideoCall,
+    HDVideoCallView,
+    HDVideoPermissionsConfiguration,
+    PreviewLocalVideoView,
+    withVideoCallPermissions
+};
