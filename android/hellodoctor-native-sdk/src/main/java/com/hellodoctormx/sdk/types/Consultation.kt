47896c54cdd5734adfca76696667daabc23ca486
Daniel Sarfati <daniel@hellodoctor.com.mx>
2022-05-25 23:55:21 -0500

    finally learned how to use mavenLocal to publish native android sdk

--- android/hellodoctor-native-sdk/src/main/java/com/hellodoctormx/sdk/types/Consultation.kt
+++
@@ -1,8 +0,0 @@
-package com.hellodoctormx.sdk.types
-
-import kotlinx.serialization.Contextual
-import kotlinx.serialization.Serializable
-import java.time.LocalDateTime
-
-@Serializable
-data class Consultation(val id: String, @Contextual val scheduledStart: LocalDateTime, val status: String)
