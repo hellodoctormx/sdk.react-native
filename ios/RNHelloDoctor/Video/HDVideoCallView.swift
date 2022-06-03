5a356c9fdcb7ef1419c3f8809d6c7f83478ca382
Daniel Sarfati <daniel@hellodoctor.com.mx>
2022-06-02 20:35:50 -0500

    minor cleanup

---
+++ ios/RNHelloDoctor/Video/HDVideoCallView.swift
@@ -0,0 +1,20 @@
+//
+//  HDVideoCallView.swift
+//  RNHelloDoctor
+//
+//  Created by HelloDoctor on 6/2/22.
+//
+
+import SwiftUI
+
+struct HDVideoCallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HDVideoCallView_Previews: PreviewProvider {
+    static var previews: some View {
+        HDVideoCallView()
+    }
+}
